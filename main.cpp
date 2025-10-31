7aba374183850c17b73840f09407580ca89a40b7
Hassan Jehangir <cadethassan2001@gmail.com>
2025-10-31 23:02:30 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,98 +1,97 @@
 #include <iostream>
 #include <vector>
-#include <iomanip>
 #include <algorithm>
 #include <numeric>
+#include <iomanip>
 using namespace std;
 
 class Person {
 private:
     string firstName;
-    string surname;
-    vector<double> homework;
-    double exam;
+    string lastName;
+    vector<int> homework;
+    int exam;
     double finalGrade;
 
 public:
-    // --- Rule of Three ---
-    Person() : exam(0), finalGrade(0) {}
-    Person(const Person& other) {
+    // Constructor
+    Person() : exam(0), finalGrade(0.0) {}
+
+    // Copy constructor
+    Person(const Person &other) {
         firstName = other.firstName;
-        surname = other.surname;
+        lastName = other.lastName;
         homework = other.homework;
         exam = other.exam;
         finalGrade = other.finalGrade;
     }
-    Person& operator=(const Person& other) {
+
+    // Assignment operator
+    Person &operator=(const Person &other) {
         if (this != &other) {
             firstName = other.firstName;
-            surname = other.surname;
+            lastName = other.lastName;
             homework = other.homework;
             exam = other.exam;
             finalGrade = other.finalGrade;
         }
         return *this;
     }
+
+    // Destructor
     ~Person() {}
 
-    friend istream& operator>>(istream& in, Person& p) {
+    // Input method
+    friend istream &operator>>(istream &in, Person &p) {
         cout << "Enter first name: ";
         in >> p.firstName;
-        cout << "Enter surname: ";
-        in >> p.surname;
+        cout << "Enter last name: ";
+        in >> p.lastName;
 
-        cout << "Enter homework results (type -1 to finish): ";
-        p.homework.clear();
-        double score;
-        while (in >> score && score != -1)
+        cout << "Enter homework results (end with -1): ";
+        int score;
+        while (true) {
+            in >> score;
+            if (score == -1) break;
             p.homework.push_back(score);
+        }
 
         cout << "Enter exam result: ";
         in >> p.exam;
         return in;
     }
 
-    friend ostream& operator<<(ostream& out, const Person& p) {
-        out << left << setw(10) << p.firstName
-            << setw(10) << p.surname
-            << setw(20) << fixed << setprecision(2)
-            << p.finalGrade;
+    // Output method
+    friend ostream &operator<<(ostream &out, const Person &p) {
+        out << left << setw(12) << p.firstName
+            << left << setw(12) << p.lastName
+            << fixed << setprecision(2) << p.finalGrade;
         return out;
     }
 
-    void calculateFinal(bool useMedian) {
-        double hwResult = 0.0;
-        if (p.homework.empty()) hwResult = 0;
-        else if (useMedian) {
-            sort(homework.begin(), homework.end());
-            int n = homework.size();
-            if (n % 2 == 0)
-                hwResult = (homework[n / 2 - 1] + homework[n / 2]) / 2.0;
-            else
-                hwResult = homework[n / 2];
-        } else {
-            hwResult = accumulate(homework.begin(), homework.end(), 0.0) / homework.size();
+    // Calculate final grade by average
+    void calculateAverage() {
+        if (homework.empty()) {
+            finalGrade = exam;
+            return;
         }
-        finalGrade = 0.4 * hwResult + 0.6 * exam;
+        double avg = accumulate(homework.begin(), homework.end(), 0.0) / homework.size();
+        finalGrade = 0.4 * avg + 0.6 * exam;
+    }
+
+    // Calculate final grade by median
+    void calculateMedian() {
+        if (homework.empty()) {
+            finalGrade = exam;
+            return;
+        }
+        sort(homework.begin(), homework.end());
+        double median;
+        int n = homework.size();
+        if (n % 2 == 0)
+            median = (homework[n / 2 - 1] + homework[n / 2]) / 2.0;
+        else
+            median = homework[n / 2];
+        finalGrade = 0.4 * median + 0.6 * exam;
     }
 };
-
-int main() {
-    int n;
-    cout << "Enter number of students: ";
-    cin >> n;
-
-    vector<Person> students(n);
-    for (int i = 0; i < n; ++i) {
-        cout << "\nStudent " << i + 1 << ":\n";
-        cin >> students[i];
-        students[i].calculateFinal(false); // false = average
-    }
-
-    cout << "\nName       Surname    Final_Point(Aver.)\n";
-    cout << "-------------------------------------------\n";
-    for (auto& s : students)
-        cout << s << endl;
-
-    return 0;
-}
